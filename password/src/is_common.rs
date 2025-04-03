382688b2af3a7fc3e69f26ba53c623875000163a
Mykyta <83033130+15traven@users.noreply.github.com>
2025-04-04 00:22:59 +0300

    clear

--- password/src/is_common.rs
+++ password/src/is_common.rs
@@ -8,8 +8,8 @@ pub fn is_common(password: &str) -> bool {
     let rockyou = File::open("password/data/rockyou.txt").expect("Failed to open rockyou.txt");
     let reader = BufReader::new(rockyou);
     let common_passwords = reader
-        .lines() // <-- This method exists and works!
-        .filter_map(Result::ok) // Filters out errors and unwraps successful lines
+        .lines()
+        .filter_map(Result::ok)
         .collect::<HashSet<String>>();
 
     common_passwords.contains(password)
